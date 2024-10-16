b10eed89382065765240ebba73463deeb12ee3bf
Ivan42210 <ivan.cha.42@outlook.fr>
2024-10-16 14:57:18 +0200

    modifications

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*toggle function*/
+
+const toggleBtn = document.getElementById('toggle-btn')
+
+toggleBtn.addEventListener('click', toggleAction)
+
+function toggleAction(e){
+    
+}
+
+console.log(toggleBtn)
