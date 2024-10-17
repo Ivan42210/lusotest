637804f1158956f3fad83dcf73ff32738e23a8cf
Ivan42210 <ivan.cha.42@outlook.fr>
2024-10-17 13:29:46 +0200

    navbar

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 /*toggle function*/
 
 const toggleBtn = document.getElementById('toggle-btn')
+const menu = document.getElementById('menu')
 
 toggleBtn.addEventListener('click', toggleAction)
 
-function toggleAction(e){
-    
+function toggleAction(){
+   
+    const menuClass = menu.classList
+
+    if(menuClass.contains('closed')){
+        menuClass.remove('closed');
+        menuClass.add('open');
+    }else{
+        menuClass.remove('open');
+        menuClass.add('closed')
+    }
 }
 
 console.log(toggleBtn)
+
+/*drawer function onClick*/
+
+const menuLinks = menu.childNodes;
+
+console.log(menuLinks)
